bafc55b0d34c30ae76175243f41f248c76e225ca
RealVirg <higlander2323@yandex.ru>
2019-10-09 12:48:11 +0500

    12:47 09.10.19

--- src/main/java/BotApp.java
+++ src/main/java/BotApp.java
@@ -2,7 +2,7 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-class App {
+class BotApp {
     public static void main(String[] args) throws TelegramApiRequestException {
         ApiContextInitializer.init();
 
